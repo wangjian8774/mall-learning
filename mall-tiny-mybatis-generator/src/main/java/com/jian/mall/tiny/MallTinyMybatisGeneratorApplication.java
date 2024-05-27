ec19f1c0950cd8b70b8b7f32d6821313e4be92de
wangjian <qiutianhuangshan@gmail.com>
2024-05-27 10:50:31 +0800

    MyBatis generator暂存

---
+++ mall-tiny-mybatis-generator/src/main/java/com/jian/mall/tiny/MallTinyMybatisGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.jian.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTinyMybatisGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallTinyMybatisGeneratorApplication.class, args);
+	}
+
+}
---
+++ mall-tiny-mybatis-generator/src/test/java/com/jian/mall/tiny/MallTinyMybatisGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jian.mall.tiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallTinyMybatisGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
