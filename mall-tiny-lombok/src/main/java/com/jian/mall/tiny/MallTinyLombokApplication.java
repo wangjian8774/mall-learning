fc10b806adbfc1feec6399b95b78914456aed372
wangjian <qiutianhuangshan@gmail.com>
2024-05-28 15:28:55 +0800

    feat(learn): lombok structure

---
+++ mall-tiny-lombok/src/main/java/com/jian/mall/tiny/MallTinyLombokApplication.java
@@ -0,0 +1,13 @@
+package com.jian.mall.tiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallTinyLombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallTinyLombokApplication.class, args);
+    }
+
+}
